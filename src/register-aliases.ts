c01d819303a4644577d576488dfba9b569c01d36
Filipe Marques <filipe@revolck.com.br>
2025-04-10 14:08:46 -0300

    Adicionando path de maneira organizada

--- src/register-aliases.ts
+++
@@ -1,7 +0,0 @@
-import moduleAlias from "module-alias";
-import path from "path";
-
-// Registra o alias @ para apontar para a pasta dist
-moduleAlias.addAliases({
-  "@": path.join(__dirname),
-});
