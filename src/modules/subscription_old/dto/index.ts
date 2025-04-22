06bd71fb71e382d280d9a05dd8b706b53c79f3db
Filipe Marques <filipe@revolck.com.br>
2025-04-22 17:09:40 -0300

    Removendo pasta desnecessaria

--- src/modules/subscription_old/dto/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Exportação de DTOs para o módulo de assinaturas
- * @module modules/subscription/dto
- */
-
-export * from "./plan.dto";
-export * from "./subscription.dto";
-export * from "./webhook.dto";
--- src/modules/subscription_old/interfaces/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Exportação de interfaces para o módulo de assinaturas
- * @module modules/subscription/interfaces
- */
-
-export * from "./plan.interface";
-export * from "./subscription.interface";
-export * from "./webhook.interface";
